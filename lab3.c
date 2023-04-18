a86aa37ace608885f7b83c18ee7d02364a1e55b9
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2023-04-18 23:04:16 +0700

    Create lab3.c
    it works incorrectly

---
+++ lab3.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+pthread_spinlock_t lock;
+pthread_spinlock_t lock1;
+pthread_spinlock_t lock2;
+char sh[6];
+int counter = 0;
+void* Thread(void* pParams);
+
+int main(void)
+{
+	pthread_t thread_id;	
+	pthread_create(&thread_id, NULL, &Thread, NULL);
+	
+	pthread_spin_init(&lock, PTHREAD_PROCESS_PRIVATE);
+	pthread_spin_init(&lock1, PTHREAD_PROCESS_PRIVATE);
+	pthread_spin_init(&lock2, PTHREAD_PROCESS_PRIVATE);
+	
+	//pthread_spinlock_t print_lock;
+	//pthread_spin_init(&print_lock, PTHREAD_PROCESS_PRIVATE);
+	
+	while(counter < 1000) 
+	{ 
+		//pthread_spin_lock(&lock1); 
+		printf("%s %d\n", sh, counter); 
+		//pthread_spin_unlock(&lock1); 
+	}	
+	
+	return 0;
+}
+
+void* Thread(void* pParams)
+{
+	
+	while (1)
+	{	
+		if(counter%2)
+		{
+			pthread_spin_lock(&lock);
+			sh[0]='H';
+			sh[1]='e';
+			sh[2]='l';
+			sh[3]='l';
+			sh[4]='o';
+			sh[5]='\0';
+			//pthread_spin_unlock(&lock);
+		}
+		else
+		{
+			pthread_spin_lock(&lock);
+			sh[0]='B';
+			sh[1]='y';
+			sh[2]='e';
+			sh[3]='_';
+			sh[4]='u';
+			sh[5]='\0';
+			//pthread_spin_unlock(&lock2);
+		}
+		//pthread_spin_lock(&lock1);
+		counter++;
+		pthread_spin_unlock(&lock);
+		//printf("-->%s %d\n", sh, counter); 
+		//usleep(0.5);
+	}
+	return NULL;
+}
