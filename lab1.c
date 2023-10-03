84d9b6234e10a54bc62558e7249b82e36365ecde
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2023-10-03 20:53:10 +0700

    Added displaying PID, PPID of processes

--- lab1.c
+++ lab1.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <dirent.h>
+
+#define READ_BUF_SIZE 512
+#define READ_PPID 6
 
 int spawn(char* program, char** arg_list)
 {
@@ -9,10 +15,7 @@ int spawn(char* program, char** arg_list)
     
     child_pid = fork();
     
-    if (child_pid != 0)
-    {
-        return child_pid;
-    }
+    if (child_pid != 0) { return child_pid; }
     else
     {
         execvp(program, arg_list);
@@ -21,20 +24,77 @@ int spawn(char* program, char** arg_list)
     }
 }
 
-int main()
+void get_pid_and_ppid(const char* pname)
 {
-    char* arg_list1[] = {"shotwell", NULL};    
-    spawn("shotwell", arg_list1);  
-     
+    char filename_status[READ_BUF_SIZE];
+    char buffer[READ_BUF_SIZE];
+    char name[READ_BUF_SIZE];
+    char ppid[READ_PPID];
+
+    FILE* status;
+
+    DIR *dir;
+    struct dirent *next;    
+
+    dir = opendir("/proc");
+    if (!dir)
+    {
+        perror("Cannot open /proc");
+        exit(-1);
+    }
+
+    printf("Name: %s\t", pname);
+
+    while ((next = readdir(dir)) != NULL)
+    {
+        if (strcmp(next -> d_name, "..") == 0) { continue; }
+        if (!isdigit(*next -> d_name)) { continue; }
+
+        sprintf(filename_status, "/proc/%s/status", next -> d_name);
+        status = fopen(filename_status, "r");
+
+        fgets(buffer, READ_BUF_SIZE - 1, status);
+        sscanf(buffer, "%*s %s", name);        
+
+        if (strcmp(pname, name) != 0) { continue; }
+        else
+        {            
+            printf("PID: %s\t", next -> d_name);
+            
+            while (fgets(buffer, READ_BUF_SIZE - 1, status) != NULL)
+            {
+                if (strstr(buffer, "PPid") != NULL)
+                {
+                    sscanf(buffer, "%*s %s", name);
+                    sscanf(buffer, "%*s %s", ppid);
+                    printf("PPID: %s\t", name);
+                    break;
+                }
+            }
+        }
+    }
+    fclose(status);
+
+    sprintf(filename_status, "/proc/%s/status", ppid);
+    status = fopen(filename_status, "r");
+
+    fgets(buffer, READ_BUF_SIZE - 1, status);
+    sscanf(buffer, "%*s %s", name); 
+    printf("Name of parent process: %s", name);
+    printf("\n");
+}
+
+int main()
+{        
+
     char* arg_list2[] = {"atom", NULL};    
     spawn("atom", arg_list2);  
+    get_pid_and_ppid("atom");
     
     char* arg_list3[] = {"emacs", NULL};    
-    spawn("emacs", arg_list3); 
-     
-    char* arg_list4[] = {"gnome-calculator", NULL};    
-    spawn("gnome-calculator", arg_list4);   
+    spawn("emacs", arg_list3);
+    get_pid_and_ppid("emacs");    
     
-    printf("done with main program\n");    
+    printf("\ndone with main program\n");    
     return 0;
 }
